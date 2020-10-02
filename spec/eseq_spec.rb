50a551048c8b19ee9f255866565c5c4eac90996e
John Mettraux <jmettraux@gmail.com>
2020-10-02 11:15:15 +0900

    Allow for empty eseqs

--- spec/eseq_spec.rb
+++ spec/eseq_spec.rb
@@ -57,6 +57,26 @@ describe 'jaabro.js' do
       )
     end
 
+    it 'hits even when empty' do
+
+      expect(js(%{
+
+        var i = Jaabro.makeInput('<>');
+        var r = Jaabro.eseq('es', i, lt, cha, com, gt);
+
+        return [ r.toArray({ leaves: true }), i.offset ];
+      })).to eq(
+        [
+          [ 'es', 1, 0, 2, 'eseq', [
+            [ nil, 1, 0, 1, 'str', '<' ],
+            [ nil, 0, 1, 0, 'rex', [] ],
+            [ nil, 1, 1, 1, 'str', '>' ],
+          ] ],
+          2
+        ]
+      )
+    end
+
     context 'no start parser' do
 
       it 'hits' do
